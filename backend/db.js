521023cf7b22b54b984977f675b56cb31ba20482
Ganeshkumar553 <ganeshkokkathi6@gmail.com>
2026-05-13 14:14:10 +0530

    Fix: Added SSL and Port for Aiven connection

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
@@ -6,9 +6,11 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
-
-module.exports = pool.promise();
